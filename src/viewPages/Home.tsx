8315156754ae0c531a686208d872d8324066c229
hnaa0 <hnaa12@daum.net>
2022-11-09 17:52:43 +0900

    Revert "item store 생성, 홈화면 아이템 리스트 구현 | redux, redux-toolkit 설치"
    This reverts commit 76cdd7717e92d4f3d405b51946fd5a0d95e87770.

--- src/viewPages/Home.tsx
+++ src/viewPages/Home.tsx
@@ -1,13 +1,9 @@
 import HomeCarousel from "../components/Carousel";
-import ItemList from "../components/ItemList";
 
 export default function Home() {
   return (
     <div>
       <HomeCarousel />
-      <ItemList category="fashion" />
-      <ItemList category="accessory" />
-      <ItemList category="digital" />
     </div>
   );
 }
