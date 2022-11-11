0361e1b9519d2d06b5b4a9d4c39b5239b566fc11
hnaa0 <hnaa12@daum.net>
2022-11-11 14:59:03 +0900

    item store 생성, 홈화면 아이템 리스트 구현 | redux, redux-toolkit 설치

--- src/viewPages/Home.tsx
+++ src/viewPages/Home.tsx
@@ -1,9 +1,13 @@
 import HomeCarousel from "../components/Carousel";
+import ItemList from "../components/ItemList";
 
 export default function Home() {
   return (
     <div>
       <HomeCarousel />
+      <ItemList category="fashion" />
+      <ItemList category="accessory" />
+      <ItemList category="digital" />
     </div>
   );
 }
